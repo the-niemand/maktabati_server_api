bf29e4ad44246fb396e8d2b7d1f8afaf27fdd006
the-niemand <149000010+the-niemand@users.noreply.github.com>
2024-12-27 13:33:33 +0100

    Update connect.js 2 for othman

--- config/connect.js
+++ config/connect.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 require('dotenv').config()
 
-mongoose.connect("mongodb+srv://zakaria:zakaria2004@maktabati.wkbkgdu.mongodb.net/?retryWrites=true&w=majority&appName=maktabati").then(()=>{
+mongoose.connect("mongodb+srv://zakaria:zakaria2004@maktabati.wkbkgdu.mongodb.net/library?retryWrites=true&w=majority&appName=maktabati").then(()=>{
      console.log('connected')
 }).catch((err)=>{
      console.log(err);
